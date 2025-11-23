4e262bd784a671cbccd7634ac2d1d955ad8ccae6
“@neliogodoi” <nelioapp@gmail.com>
2025-11-23 17:26:06 -0300

    fix project runner

---
+++ .angular/cache/21.0.0/app/vite/deps/firebase_app.js
@@ -0,0 +1,47 @@
+import {
+  DEFAULT_ENTRY_NAME,
+  FirebaseError,
+  SDK_VERSION,
+  _addComponent,
+  _addOrOverwriteComponent,
+  _apps,
+  _clearComponents,
+  _components,
+  _getProvider,
+  _registerComponent,
+  _removeServiceInstance,
+  deleteApp,
+  getApp,
+  getApps,
+  initializeApp,
+  onLog,
+  registerVersion,
+  setLogLevel
+} from "./chunk-OI7V4SKC.js";
+import "./chunk-GOMI4DH3.js";
+
+// node_modules/firebase/app/dist/esm/index.esm.js
+var name = "firebase";
+var version = "9.23.0";
+registerVersion(name, version, "app");
+export {
+  FirebaseError,
+  SDK_VERSION,
+  DEFAULT_ENTRY_NAME as _DEFAULT_ENTRY_NAME,
+  _addComponent,
+  _addOrOverwriteComponent,
+  _apps,
+  _clearComponents,
+  _components,
+  _getProvider,
+  _registerComponent,
+  _removeServiceInstance,
+  deleteApp,
+  getApp,
+  getApps,
+  initializeApp,
+  onLog,
+  registerVersion,
+  setLogLevel
+};
+//# sourceMappingURL=firebase_app.js.map
